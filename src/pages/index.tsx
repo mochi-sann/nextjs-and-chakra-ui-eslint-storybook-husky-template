92c739381e45d5eabfb5f13e91712fff130f692e
mochi-sann <moyurusuto@gmail.com>
2021-02-21 12:26:10 +0900

    huskyの設定を修正2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import { FaReact } from "react-icons/fa"
 import Layout from "~/src/components/Layout"
 import { CTA } from "~/src/components/CTA"
 
-import { DarkModeSwitch } from "../components/DarkModeSwitch"
+
 
 const Index = () => (
   <Layout>
@@ -18,11 +18,25 @@ const Index = () => (
 
     <Heading>
       nextjs-and-chakra-ui-eslint-storybook-husky-template
-    
+      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
+    </Heading>
+    <Heading>
+      nextjs-and-chakra-ui-eslint-storybook-husky-template
+      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
+    </Heading>
+    <Heading>
+      nextjs-and-chakra-ui-eslint-storybook-husky-template
+      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
+    </Heading>
+    <Heading>
+      nextjs-and-chakra-ui-eslint-storybook-husky-template
+      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
+    </Heading>
+    <Heading>
+      nextjs-and-chakra-ui-eslint-storybook-husky-template
       <Icon as={FaReact} w={10} h={10} color="#0693E3" />
     </Heading>
 
-    <DarkModeSwitch />
     {/* <Footer>
       <Text>Next ❤️ Chakra</Text>
     </Footer> */}
