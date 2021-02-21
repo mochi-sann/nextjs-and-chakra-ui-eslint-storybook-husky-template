f003eb333749980e2f8b4555279c1eb408bf0d2d
mochi-sann <moyurusuto@gmail.com>
2021-02-21 12:31:34 +0900

    諸々変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,13 @@
-import {
-
-  Heading,
-
-
-  Icon,
-} from "@chakra-ui/react"
+import { Heading, Icon } from "@chakra-ui/react"
 import { FaReact } from "react-icons/fa"
 
 import Layout from "~/src/components/Layout"
 import { CTA } from "~/src/components/CTA"
 
-
-
 const Index = () => (
   <Layout>
     {/* <Hero /> */}
 
-    <Heading>
-      nextjs-and-chakra-ui-eslint-storybook-husky-template
-      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
-    </Heading>
-    <Heading>
-      nextjs-and-chakra-ui-eslint-storybook-husky-template
-      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
-    </Heading>
-    <Heading>
-      nextjs-and-chakra-ui-eslint-storybook-husky-template
-      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
-    </Heading>
-    <Heading>
-      nextjs-and-chakra-ui-eslint-storybook-husky-template
-      <Icon as={FaReact} w={10} h={10} color="#0693E3" />
-    </Heading>
     <Heading>
       nextjs-and-chakra-ui-eslint-storybook-husky-template
       <Icon as={FaReact} w={10} h={10} color="#0693E3" />
